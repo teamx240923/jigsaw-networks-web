4a149f0bca095802ce94f230dea98b14bf442353
Sanjay B <sanjayjothi2000@gmail.com>
2025-09-16 23:48:11 +0530

    fix: favicon, logo

--- src/features/home/components/header.tsx
+++ src/features/home/components/header.tsx
@@ -3,6 +3,8 @@
 import { motion } from 'framer-motion';
 import { Menu, X } from 'lucide-react';
 import { useState } from 'react';
+import AppIcon from '@public/icon.svg'
+import NextImage from 'next/image';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -35,8 +37,9 @@ export default function Header() {
           {/* Logo */}
           <motion.div
             whileHover={{ scale: 1.05 }}
-            className="flex-shrink-0"
-          >
+            className="flex-shrink-0 flex items-center gap-2"
+          >                               
+            <NextImage src={AppIcon} width={32} height={32} alt='App Logo' />
             <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
               Jigsaw Networks
             </h1>
